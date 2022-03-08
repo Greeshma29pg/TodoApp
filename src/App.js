f26519abd7b15cf7135deb20eb8ac445c6d2714c
Greeshma29pg <77615013+Greeshma29pg@users.noreply.github.com>
2022-03-08 12:48:28 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import Resturant from './components/Basics/Resturant'
-// import UseState from './components/Hooks/useState'
-// import UseEffect from './components/Hooks/useEffect'
-// import UseReducer from './components/Hooks/useReducer'
 import Todo from './components/todoList/todo'
-// import Temp from './components/weather/temp'
+
 
 const App = () => {
-  // return <Resturant/>
-  // return <UseState/>
-  // return <UseEffect/>
-  // return <UseReducer/>
   return <Todo/>
-  // return <Temp/>
 }
 export default App
 
